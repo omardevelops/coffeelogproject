58a9d8acbb438ef5bf868201dd2d7963125c06e6
Omar Al Blooshi <omarahmadp91@gmail.com>
2025-06-10 23:34:18 +0400

    fixed sorting order for brews list

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,9 @@ function App() {
 
   console.log('Object with ID 1:', getObjectById('9743408e-c1bd-4abf-9314-b431f1ba6ef0'));
 
+  // Sort brews by creation date descending (bean.config.unix_timestamp)
+  brews.sort((a, b) => b.config.unix_timestamp - a.config.unix_timestamp);
+
   // Render the Beans JSON in a nice looking way as a display flex of items
   // I just want to render the brews list in a display flex format with the ID and name of each brew
   // It should be styled with TailwindCSS to look nice and modern and minimalistic with a dark theme and consolas font
